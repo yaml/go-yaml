37aef96d8984888655c41c5a420e31c8d22a699f
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-08-18 15:57:45 +0200

    chore: move package comment to doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package yaml implements YAML support for the Go language.
+//
+// Source code and other details for the project are available at GitHub:
+//
+//	https://github.com/yaml/go-yaml
+package yaml
